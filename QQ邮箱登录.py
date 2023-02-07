9189ba6b1a228c7fc3840fbffdbd8b08b95ab1a0
panmeibing <53526421+panmeibing@users.noreply.github.com>
2023-02-07 11:58:06 +0800

    Update QQ邮箱登录.py
    selenium使用4.0之后的语法

--- QQ邮箱登录.py
+++ QQ邮箱登录.py
@@ -1,5 +1,7 @@
 import sys
 import time
+from io import BytesIO
+
 import cv2
 import os
 import requests
@@ -8,124 +10,124 @@ import numpy as np
 from PIL import Image as Im
 from selenium import webdriver
 from selenium.webdriver import ActionChains
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+wait_time_short = 1
+wait_time1_long = 2
 
 
 class QQEmail(object):
-    def __init__(self, user, pwd):
-        self.root = sys.path[0] + "/pic/"
-        self.driver = webdriver.Firefox()
-        self.user = user  # 用户名
-        self.pwd = pwd  # 密码
+    def __init__(self, user, pwd, executable_path):
+        self.user = user
+        self.pwd = pwd
+        self.driver = webdriver.Chrome(service=Service(executable_path))
+        self.save_dir = sys.path[0] + "/pic/"
 
     def login_email(self):
         login_url = "https://mail.qq.com"
-        time.sleep(2)
         self.driver.get(login_url)
-        time.sleep(2)
-
+        time.sleep(wait_time1_long)
         self.driver.switch_to.frame("login_frame")
         try:
-            self.driver.find_element_by_id("switcher_plogin").click()
+            self.driver.find_element("switcher_plogin").click()
         except Exception as e:
             print(e)
-        time.sleep(1)
+        time.sleep(wait_time1_long)
 
-        u_input = self.driver.find_element_by_id("u")
+        u_input = self.driver.find_element(By.XPATH, '//*[@id="u"]')
         u_input.clear()
         u_input.send_keys(self.user)
-        time.sleep(1)
-        p_input = self.driver.find_element_by_id("p")
+        time.sleep(wait_time_short)
+        p_input = self.driver.find_element(By.XPATH, '//*[@id="p"]')
         p_input.clear()
         p_input.send_keys(self.pwd)
-        time.sleep(1)
-        self.driver.find_element_by_id("login_button").click()
-        time.sleep(2)
-
-        if "tcaptcha_iframe" in self.driver.page_source:
-            self.driver.switch_to.frame("tcaptcha_iframe")
-
+        time.sleep(wait_time_short)
+        self.driver.find_element(By.ID, "login_button").click()
+        time.sleep(wait_time1_long)
+        if "tcaptcha_iframe_dy" in self.driver.page_source:
+            self.driver.switch_to.frame("tcaptcha_iframe_dy")
         if "拖动下方滑块完成拼图" in self.driver.page_source:
-            print("拖动下方滑块完成拼图")
-
-            self.verification_code(23)
+            print("需要验证码")
+            self.verification_code(60)
         else:
             print("不需要验证码，直接登录")
+        time.sleep(10)
 
-    def download_img(self, url, pic_type):
-        path = self.root + pic_type + '.png'
-        try:
-            if not os.path.exists(self.root):
-                os.makedirs(self.root)
-            res = requests.get(url)
-            with open(path, "wb") as f:
-                f.write(res.content)
-            return f.name
-        except Exception as e:
-            print(e)
-            self.download_img(url, pic_type)
+    def download_img(self, url, size_type):
+        try_times = 3
+        path = self.save_dir + size_type + '.png'
+        while try_times > 0:
+            try:
+                if not os.path.exists(self.save_dir):
+                    os.makedirs(self.save_dir)
+                res = requests.get(url)
+                if size_type == "small":
+                    img = Im.open(BytesIO(res.content))
+                    crop = img.crop((140, 490, 260, 610))
+                    crop.save(path)
+                    return path
+                else:
+                    with open(path, "wb") as f:
+                        f.write(res.content)
+                    return f.name
+            except Exception as e:
+                print(e)
+                try_times -= 1
 
     def get_distance(self, small_url, big_url):
-
         s_img = self.download_img(small_url, 'small')
         time.sleep(2)
         b_img = self.download_img(big_url, 'big')
-
-        target = cv2.imread(s_img, 0)
-        template = cv2.imread(b_img, 0)
-        w, h = target.shape[::-1]
-        temp = self.root + 'temp.jpg'
-        targ = self.root + 'targ.jpg'
-        cv2.imwrite(temp, template)
-        cv2.imwrite(targ, target)
-        target = cv2.imread(targ)
-        target = cv2.cvtColor(target, cv2.COLOR_BGR2GRAY)
-        target = abs(255 - target)
-        cv2.imwrite(targ, target)
-        target = cv2.imread(targ)
-        template = cv2.imread(temp)
-        result = cv2.matchTemplate(target, template, cv2.TM_CCOEFF_NORMED)
-        x, y = np.unravel_index(result.argmax(), result.shape)
-        image = Im.open(b_img)
-        xy = (y + 20, x + 20, y + w - 20, x + h - 20)
-        imagecrop = image.crop(xy)
-        imagecrop.save(self.root + "/new_image.jpg")
+        print("s_img:", s_img)
+        print("b_img:", b_img)
+        small_pic = cv2.Canny(cv2.GaussianBlur(cv2.imread('pic/small.png'), (5, 5), 0), 100, 300)
+        big_pic = cv2.Canny(cv2.GaussianBlur(cv2.imread('pic/big.png'), (5, 5), 0), 100, 300)
+        res = cv2.matchTemplate(big_pic, small_pic, cv2.TM_CCOEFF_NORMED)
+        x, y = np.unravel_index(res.argmax(), res.shape)
+        # min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+        # print('max_loc[0]:', max_loc[0])
         return y
 
     def verification_code(self, offset):
         self.driver.switch_to.default_content()
         self.driver.switch_to.frame('login_frame')
-        self.driver.switch_to.frame(self.driver.find_element_by_id("tcaptcha_iframe"))
-        self.driver.find_element_by_id('e_reload').click()
+        self.driver.switch_to.frame(self.driver.find_element(By.ID, "tcaptcha_iframe_dy"))
+        self.driver.find_element(By.ID, 'e_reload').click()
+        time.sleep(wait_time1_long)
+        small_pic_pattern = '//*[@id="tcOperation"]/div[8]'
+        small_url = self.driver.find_element(By.XPATH, small_pic_pattern).value_of_css_property('background-image')
+        small_url = small_url.split('"')[1]
+        print("small_url:", small_url)
+        big_pic_pattern = '/html/body/div/div[3]/div[2]/div[1]/div[2]/div'
+        big_url = self.driver.find_element(By.XPATH, big_pic_pattern).value_of_css_property('background-image')
+        big_url = big_url.split('"')[1]
+        print(big_url)
 
-        small_url = self.driver.find_element_by_id('slideBlock').get_attribute('src')
-        big_url = self.driver.find_element_by_id('slideBg').get_attribute('src')
-        y = self.get_distance(small_url, big_url)
-
-        element = self.driver.find_element_by_id('tcaptcha_drag_button')
-        distance = y * (280.0 / 680.0) - offset
-        print('distance:', distance)
-        has_gone_dist = 0
-        remaining_dist = distance
+        distance = self.get_distance(small_url, big_url)
+        distance = (distance - offset) * (280.0 / 680.0)
+        print("distance:", distance)
+        element = self.driver.find_element(By.XPATH, '//*[@id="tcOperation"]/div[6]')
         ActionChains(self.driver).click_and_hold(element).perform()
         time.sleep(0.5)
-
         steps = self.get_steps(distance)
+        print("steps:", steps)
         for step in steps:
-            print(step)
             ActionChains(self.driver).move_by_offset(step, random.randint(-5, 5)).perform()
             time.sleep((random.randint(5, 15) / 1000))
 
         ActionChains(self.driver).release(on_element=element).perform()
-
+        time.sleep(wait_time1_long)
         self.driver.switch_to.parent_frame()
+        time.sleep(wait_time1_long)
         if self.driver.title != "QQ邮箱":
             try:
                 self.driver.switch_to.default_content()
                 self.driver.switch_to.frame('login_frame')
-                self.driver.switch_to.frame("tcaptcha_iframe")
-                print(self.driver.find_element_by_id('guideText').text)
+                self.driver.switch_to.frame("tcaptcha_iframe_dy")
+                print(self.driver.find_element(By.ID, 'guideText').text)
                 print('滑动失败!')
-                self.verification_code(23)
+                self.verification_code(60)
             except Exception as e:
                 print(e)
                 print('帐号密码有误!')
@@ -135,7 +137,7 @@ class QQEmail(object):
             self.driver.quit()
 
     def get_steps(self, dis):
-        # v=v0+at, s=v0t+½at², v²-v0²=2as
+        # 计算公式：v=v0+at, s=v0t+½at², v²-v0²=2as
         v = 0
         t = 0.3
         steps = []
@@ -153,7 +155,14 @@ class QQEmail(object):
             v = v0 + a * t
         return steps
 
+    def __del__(self):
+        if self.driver:
+            self.driver.quit()
+
 
 if __name__ == '__main__':
-    email = QQEmail("993226448", "23562465")
+    driver_path = r"E:\webdriver\chromedriver.exe"
+    user_name = '993226449'
+    password = '123456789'
+    email = QQEmail(user_name, password, driver_path)
     email.login_email()
